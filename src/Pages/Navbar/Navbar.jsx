c050b25dc79b4a2076fd452eabd8e5f3824f8b1f
RF-Rifat <rfrifat6344@gmail.com>
2024-01-02 15:10:21 +0600

    dark mode and light mode enabled

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
 
     return (
         <>
-            <div className="navbar bg-gray-200 rounded-md">
+            <div className="navbar bg-secondary-content rounded-md">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
